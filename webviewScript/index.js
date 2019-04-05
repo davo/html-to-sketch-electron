908e1ff4df64e6e63256053180c44cf8cecbc0c9
Davo Galavotti <davidgalavotti@gmail.com>
2019-04-05 12:04:44 -0400

    Poking into this beast

--- webviewScript/index.js
+++ webviewScript/index.js
@@ -1,5 +1,6 @@
-import page2layers from './page2layers';
-import {startPicker, stopPicker} from './picker';
-import findFont from './findFont';
+import page2layers from './page2layers'
+import { startPicker, stopPicker } from './picker'
+import findFont from './findFont'
+import findOpen from './findOpen'
 
-export {page2layers, startPicker, stopPicker, findFont};
+export { page2layers, startPicker, stopPicker, findFont }
